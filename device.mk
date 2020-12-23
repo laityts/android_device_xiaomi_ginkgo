trinket-devs/device_xiaomi_ginkgo

[Squashed] ginkgo: Import XiaomiParts
From bfcc56f8f80fb4314389dccb40f46998de8d6bb5 Mon Sep 17 00:00:00 2001
From: Bruno Martins <bgcngm@gmail.com>
Date: Mon, 16 Sep 2019 17:25:48 +0200
Subject: [PATCH] sdm660-common: XiaomiParts: Migrate to androidx

 * Cleanup some unneded files

From 08784fd3140ef3621522086d9d7e136a38ca93da Mon Sep 17 00:00:00 2001
From: darkbeast69 <guptaaryan189@gmail.com>
Date: Tue, 15 Oct 2019 10:58:29 +0000
Subject: [PATCH] sdm660: Add Vector Icons to XiaomiParts

 * It looks dope icons from https://materialdesignicons.com

From 702f8d64b9105f7b68babb1551aee43b6e5960d3 Mon Sep 17 00:00:00 2001
From: Aryan <guptaaryan189@gmail.com>
Date: Sat, 19 Oct 2019 07:45:46 +0000
Subject: [PATCH] sdm660:Make vector images use the same accent color as the rest of the system!

From d9cde63c35370fb9541a277a34f062b1170c0071 Mon Sep 17 00:00:00 2001
From: Stylogey <stylogey@gmail.com>
Date: Sat, 19 Oct 2019 17:20:42 +0200
Subject: [PATCH] sdm660-common: XiaomiParts: Change settings icon to MI logo

From 197013de2eacd66674e38dcaddbb22ea5b4bace1 Mon Sep 17 00:00:00 2001
From: Stylogey <stylogey@gmail.com>
Date: Sat, 19 Oct 2019 19:04:14 +0200
Subject: [PATCH] sdm660-common: XiaomiParts: Add a preview image to kcal

From 96cf920455d1c59e5acf9c55f58c36bead734905 Mon Sep 17 00:00:00 2001
From: Stylogey <stylogey@gmail.com>
Date: Sat, 19 Oct 2019 19:10:00 +0200
Subject: [PATCH] sdm660-common: XiaomiParts: Dont crash if dirac is not initialized

From 356f5131029256b5d16bb402222ff9e37bd8ab7c Mon Sep 17 00:00:00 2001
From: Dušan Uverić <dusan.uveric9@gmail.com>
Date: Wed, 15 Apr 2020 21:14:49 +0200
Subject: [PATCH] sdm660-common: XiaomiParts: check if vibrator strength is supported before adding

Co-authored-by: Bruno Martins <bgcngm@gmail.com>
Co-authored-by: darkbeast69 <guptaaryan189@gmail.com>
Co-authored-by: Aryan <guptaaryan189@gmail.com>
Co-authored-by: Dušan Uverić <dusan.uveric9@gmail.com>
Co-authored-by: Mrinal Ghosh <mg712702@gmail.com>
Signed-off-by: Mrin

device.mk
@@ -369,6 +369,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.target.rc \
+    init.xiaomi_parts.rc \
     ueventd.qcom.rc \
     fstab.qcom
 

laityts/android_device_xiaomi_ginkgo

device.mk
@@ -87,7 +87,8 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.target.rc
+    init.target.rc \
+    init.xiaomi_parts.rc
 
 # Sensors
 PRODUCT_PACKAGES += \